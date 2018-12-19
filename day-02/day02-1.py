1800df557de3397058bb26eeb411504a27aacaa9
jthomas <jthomas@westervelt.com>
2018-12-19 09:19:33 -0600

    revert

--- day-02/day02-1.py
+++ day-02/day02-1.py
@@ -4,6 +4,5 @@ for line in f:
     letters = list(line.rstrip())
     print(letters)
     for letter in letters:
-        if line.count(letter) == 2:
+        if line.count(letter) > 1:
             print("letter " + str(letter) + " has " + str(line.count(letter)) + " occurence(s).")
-            
