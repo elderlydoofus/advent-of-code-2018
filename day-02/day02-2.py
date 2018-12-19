659d1a7dcd831611cfffa52e2a0cdde9ac27fba6
jthomas <jthomas@westervelt.com>
2018-12-19 16:25:04 -0600

    working code! though I stole some inspiration from the reddit thread

--- day-02/day02-2.py
+++ day-02/day02-2.py
@@ -2,8 +2,9 @@ import itertools
 
 f = open("day-02\\input.txt", "r")
 contents = f.read().strip().splitlines()
-
-for x, y in itertools.product(contents, contents):
-    compare = frozenset(x).difference(frozenset(y))
-    if len(compare) == 1:
-        print(frozenset(x).intersection(frozenset(y)))
+ 
+for x, y in itertools.product(contents, repeat=2):
+    diff = [i for i,j in zip(x,y) if i == j]
+    if len(y)-len(diff) == 1:
+        print("".join(diff))
+        break
