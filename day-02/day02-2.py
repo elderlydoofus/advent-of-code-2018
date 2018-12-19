3694eca5d579e189b868ef8d6a209ee2a2ade220
jthomas <jthomas@westervelt.com>
2018-12-19 15:52:34 -0600

    non working code when real input

--- day-02/day02-2.py
+++ day-02/day02-2.py
@@ -1,9 +1,9 @@
 import itertools
 
-f = open("day-02\\test_input.txt", "r")
+f = open("day-02\\input.txt", "r")
 contents = f.read().strip().splitlines()
 
 for x, y in itertools.product(contents, contents):
     compare = frozenset(x).difference(frozenset(y))
     if len(compare) == 1:
-        print(x)
+        print(frozenset(x).intersection(frozenset(y)))
