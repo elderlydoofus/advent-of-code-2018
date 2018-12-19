d722a7bf9890d2e6b621c1748d5a15940dbf811e
jthomas <jthomas@westervelt.com>
2018-12-19 13:54:43 -0600

    one loop through working

--- day-02/day02-2.py
+++ day-02/day02-2.py
@@ -1,5 +1,9 @@
 f = open("day-02\\test_input.txt", "r")
 
 for line in f:
-    for char in list(line.rstrip()):
-        print(line.index(char))
+    line_a = line.rstrip()
+    for line in f:
+        line_b = line.rstrip()
+        line_comp = line_a + line_b
+        line_set = set(line_comp)
+        print(line_set)
