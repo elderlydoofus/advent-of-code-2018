465f76d014263664b6bfff545f2823b6c4419743
jthomas <jthomas@westervelt.com>
2018-12-19 14:25:57 -0600

    refactord

--- day-02/day02-2.py
+++ day-02/day02-2.py
@@ -1,9 +1,6 @@
 f = open("day-02\\test_input.txt", "r")
+contents = f.read().strip().splitlines()
 
-for line in f:
-    line_a = line.rstrip()
-    for line in f:
-        line_b = line.rstrip()
-        line_comp = line_a + line_b
-        line_set = set(line_comp)
-        print(line_set)
+for linex in contents:
+    for liney in contents:
+        print(set(linex + liney))
