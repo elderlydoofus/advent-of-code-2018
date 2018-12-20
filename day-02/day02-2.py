166af7a141328ec9caeb5fb63c8908ff5e766c5c
jthomas <jthomas@westervelt.com>
2018-12-20 10:56:39 -0600

    import only product from itertools

--- day-02/day02-2.py
+++ day-02/day02-2.py
@@ -1,9 +1,9 @@
-import itertools
+from itertools import product
 
 f = open("day-02\\input.txt", "r")
 contents = f.read().strip().splitlines()
  
-for x, y in itertools.product(contents, repeat=2):
+for x, y in product(contents, repeat=2):
     diff = [i for i,j in zip(x,y) if i == j]
     if len(y)-len(diff) == 1:
         print("".join(diff))
