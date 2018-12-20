682cecb95310a2eafc8b77ec165389e678768117
jthomas <jthomas@westervelt.com>
2018-12-20 11:36:24 -0600

    capture multiple matched digits instead of just the first one

--- day-03/day03-1.py
+++ day-03/day03-1.py
@@ -1,10 +1,10 @@
 from re import findall
 
-f = open("day-03\\test_input.txt", "r")
+f = open("day-03\\input.txt", "r")
 contents = f.read().strip().splitlines()
 
-idnum = findall(r"(?<=\#)(\d)", str(contents))
-x = findall(r"(?<=\@\s)(\d)", str(contents))
-y = findall(r"(?<=\,)(\d)", str(contents))
-i = findall(r"(?<=\:\s)(\d)", str(contents))
-j = findall(r"(?<=[x])(\d)", str(contents))
+idnum = findall(r"(?<=\#)(\d+)", str(contents))
+x = findall(r"(?<=\@\s)(\d+)", str(contents))
+y = findall(r"(?<=\,)(\d+)", str(contents))
+i = findall(r"(?<=\:\s)(\d+)", str(contents))
+j = findall(r"(?<=[x])(\d+)", str(contents))
