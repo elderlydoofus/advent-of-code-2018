b0f5d1270267c53d1891acae3cf0a2034a3cb093
jthomas <jthomas@westervelt.com>
2018-12-20 11:31:52 -0600

    change import to only findall

--- day-03/day03-1.py
+++ day-03/day03-1.py
@@ -1,10 +1,10 @@
-import re
+from re import findall
 
 f = open("day-03\\test_input.txt", "r")
 contents = f.read().strip().splitlines()
 
-idnum = re.findall(r"(?<=\#)(\d)", str(contents))
-x = re.findall(r"(?<=\@\s)(\d)", str(contents))
-y = re.findall(r"(?<=\,)(\d)", str(contents))
-i = re.findall(r"(?<=\:\s)(\d)", str(contents))
-j = re.findall(r"(?<=[x])(\d)", str(contents))
+idnum = findall(r"(?<=\#)(\d)", str(contents))
+x = findall(r"(?<=\@\s)(\d)", str(contents))
+y = findall(r"(?<=\,)(\d)", str(contents))
+i = findall(r"(?<=\:\s)(\d)", str(contents))
+j = findall(r"(?<=[x])(\d)", str(contents))
